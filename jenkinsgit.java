678574a4bb428d94da932194160891193a2339cb
Aravindh <163547834+Imaravindh@users.noreply.github.com>
2024-04-30 07:57:06 +0530

    Update jenkinsgit.java

--- jenkinsgit.java
+++ jenkinsgit.java
@@ -1,5 +1,5 @@
 public class jenkinsgit{
 public static void main(String[] args){
-System.out.println("Jenkis trigger based on the new Git commits and new push. Initial commits....");
+System.out.println("Jenkis trigger based on the new Git commits and new push. Initial commits...........");
 }
 }
